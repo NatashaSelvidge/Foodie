12c012332a1d3be9ecef075721519c39e589a6ae
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-01-04 10:07:37 -0800

    added api

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,36 @@
-class RestaurantController < ApplicationController
+class RestaurantsController < ApplicationController
 
   #CREATE
 
+      # New (form)
+      # make a get request to '/restaurants/new'
+
+      # Create 
+      # make a post request to '/restaurants'
+
   #READ
-  
+
+      #Index
+      #make a get request to '/restaurants'
+  get '/restaurants' do 
+    @restaurants = Restaurant.all
+    erb :'restaurants/index'
+  end 
+
+      #Show
+      # make a get request ot '/restaurants/:id'
+
+
   #UPDATE
+
+      # Edit
+      # make a get request to 'restaurants/:id/edit' 
+
+      # Update
+      # make a post (patch) request to '/restaurants/:id'
   
   #DELETE
+
+    # make a post (delete) request to '/restaurants/:id'
 
 end 
---
+++ app/models/api.rb
@@ -0,0 +1,23 @@
+require 'net/http'
+require 'uri'
+
+class Api < ActiveRecord::Base
+
+    def self.fetch(search)
+        uri = URI.parse(search)
+        request = Net::HTTP::Get.new(uri)
+        request["Accept"] = "application/json"
+        request["User-Key"] = "7ca5836b3109fa9a24db90a0e1e19f69"
+        req_options = {
+          use_ssl: uri.scheme == "https",
+        }
+        response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+          http.request(request)
+        end
+        JSON.parse(response.body)
+      end
+   
+end
+
+
+# API Key "7ca5836b3109fa9a24db90a0e1e19f69"
